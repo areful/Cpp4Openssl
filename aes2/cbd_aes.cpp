88ab3ec4bff6b12b4ed666198a7438e4191ce593
areful <areful1997@163.com>
2020-08-25 23:22:46 -0700

    optimize code of aes2

--- aes2/cbd_aes.cpp
+++ aes2/cbd_aes.cpp
@@ -5,85 +5,71 @@
 #include "cbd_aes.h"
 #include "../b64v2/base64.h"
 #include <openssl/aes.h>
-#include <cstring>
 #include <string>
 
 using namespace std;
 
-static string private_encrypt(string &content, unsigned char *key, unsigned char *iv);
+static unsigned char *private_encrypt(unsigned char *input, int in_len, int type);
 
-static string private_decrypt(string &content, unsigned char *key, unsigned char *iv);
+static char *private_decrypt(unsigned char *in, unsigned int in_len, unsigned int out_len, int type);
 
 string encrypt(string &content, int type) {
-    int size = AES_BLOCK_SIZE;
-    unsigned char key[AES_BLOCK_SIZE + 1] = {0};
-    unsigned char iv[AES_BLOCK_SIZE] = {0};
-    if (type == 1) {
-        memcpy(key, KEY_HTTP, size);
-        memcpy(iv, IV_HTTP, size);
-    } else if (type == 2) {
-        memcpy(key, KEY_PASSWORD, size);
-        memcpy(iv, IV_PASSWORD, size);
-    } else {
-        memcpy(key, KEY_TRACK, size);
-        memcpy(iv, IV_TRACK, size);
-    }
-    return private_encrypt(content, key, iv);
-}
-
-static string private_encrypt(string &content, unsigned char *key, unsigned char *iv) {
-    AES_KEY aes;
-    if (AES_set_encrypt_key((unsigned char *) key, KEY_SIZE, &aes) < 0) {
+    int in_len = content.length();
+    auto *out = private_encrypt((unsigned char *) content.c_str(), in_len, type);
+    if (out == nullptr) {
         return std::string();
     }
 
-    int in_len = content.length();
     unsigned int out_len = in_len + (AES_BLOCK_SIZE - in_len % AES_BLOCK_SIZE);
-
-    auto *input = (unsigned char *) calloc(1, out_len + 1);
-    strncpy((char *) input, content.c_str(), in_len);
-
-    auto *out = (unsigned char *) malloc(out_len);
-    AES_cbc_encrypt(input, out, out_len, &aes, iv, AES_ENCRYPT);
     char *res = b64_encode(out, out_len);
     return std::string(res);
 }
 
-string decrypt(string &content, int type) {
-    int size = AES_BLOCK_SIZE;
+static unsigned char *private_encrypt(unsigned char *input, int in_len, int type) {
+    AES_KEY aes;
     unsigned char key[AES_BLOCK_SIZE + 1] = {0};
     unsigned char iv[AES_BLOCK_SIZE] = {0};
-    if (type == 1) {
-        memcpy(key, KEY_HTTP, size);
-        memcpy(iv, IV_HTTP, size);
-    } else if (type == 2) {
-        memcpy(key, KEY_PASSWORD, size);
-        memcpy(iv, IV_PASSWORD, size);
-    } else {
-        memcpy(key, KEY_TRACK, size);
-        memcpy(iv, IV_TRACK, size);
+
+    get_key_iv(key, iv, AES_BLOCK_SIZE, type);
+    if (AES_set_encrypt_key(key, KEY_SIZE, &aes) < 0) {
+        return nullptr;
     }
-    return private_decrypt(content, key, iv);
+
+    unsigned int out_len = in_len + (AES_BLOCK_SIZE - in_len % AES_BLOCK_SIZE);
+    auto *out = (unsigned char *) malloc(out_len);
+    AES_cbc_encrypt(input, out, out_len, &aes, iv, AES_ENCRYPT);
+    return out;
 }
 
-static string private_decrypt(string &content, unsigned char *key, unsigned char *iv) {
+string decrypt(string &content, int type) {
     if (content.empty()) {
         return std::string();
     }
 
-    char *b64_encrypted_data = const_cast<char *>(content.c_str());
+    const char *b64_encrypted_data = content.c_str();
     int in_len = content.length();
-
     size_t out_len;
     size_t *p_out_len = &out_len;
-    auto *in = b64_decode_ex(b64_encrypted_data, in_len, reinterpret_cast<size_t *>(p_out_len));
-
-    AES_KEY aes;
-    if (AES_set_decrypt_key(key, KEY_SIZE, &aes) < 0) {
+    auto *in = b64_decode_ex(b64_encrypted_data, in_len, p_out_len);
+    char *out = private_decrypt(in, in_len, out_len, type);
+    if (out == nullptr) {
         return std::string();
     }
+    return string(out);
+}
 
-    int total = in_len + (AES_BLOCK_SIZE - in_len % AES_BLOCK_SIZE);
+static char *private_decrypt(unsigned char *in, unsigned int in_len, unsigned int out_len, int type) {
+    AES_KEY aes;
+    int size = AES_BLOCK_SIZE;
+    unsigned char key[AES_BLOCK_SIZE + 1] = {0};
+    unsigned char iv[AES_BLOCK_SIZE] = {0};
+
+    get_key_iv(key, iv, size, type);
+    if (AES_set_decrypt_key(key, KEY_SIZE, &aes) < 0) {
+        return nullptr;
+    }
+
+    unsigned int total = in_len + (AES_BLOCK_SIZE - in_len % AES_BLOCK_SIZE);
     auto *out = (unsigned char *) malloc(total);
     AES_cbc_encrypt(in, out, total, &aes, iv, AES_DECRYPT);
 
@@ -93,5 +79,5 @@ static string private_decrypt(string &content, unsigned char *key, unsigned char
     }
     char *result = (char *) calloc(out_len + 1, 1);
     strncpy(result, (char *) out, out_len);
-    return std::string(result);
+    return result;
 }
--- aes2/cbd_aes.h
+++ aes2/cbd_aes.h
@@ -9,6 +9,7 @@
 
 #include <string>
 #include <openssl/aes.h>
+#include <cstring>
 
 static unsigned char KEY_HTTP[AES_BLOCK_SIZE + 1] = "EF290D911DD34E8E";
 static unsigned char IV_HTTP[AES_BLOCK_SIZE] = {
@@ -27,6 +28,19 @@ static unsigned char IV_TRACK[AES_BLOCK_SIZE] = {
         0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF,
         0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF
 };
+
+static void get_key_iv(unsigned char *key, unsigned char *iv, int size, int type) {
+    if (type == 1) {
+        memcpy(key, KEY_HTTP, size);
+        memcpy(iv, IV_HTTP, size);
+    } else if (type == 2) {
+        memcpy(key, KEY_PASSWORD, size);
+        memcpy(iv, IV_PASSWORD, size);
+    } else {
+        memcpy(key, KEY_TRACK, size);
+        memcpy(iv, IV_TRACK, size);
+    }
+}
 
 std::string encrypt(std::string &content, int type);
 
